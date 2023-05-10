67b93707f4552e1fe9ba4b880326563e9701ce4e
gonzam77 <gonzam77@gmial.com>
2023-05-10 20:07:45 -0300

    app modificado

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
+import NavBar from "./Components/NavBar";
+import Home from "./Pages/Home";
+import Landing from "./Pages/Landing/index.jsx";
 
-import './App.css';
-
-// importar componentes
 function App() {
   return (
-    <>
-    </>
-  )
+    <div className="App">
+      <header></header>
+      <NavBar/>
+      <main>
+      <Routes>
+        <Route exact path='/' element ={<Landing/>}/>
+        <Route exact path='/home' element ={<Home/>}/> 
+      </Routes>
+      </main>
+      <footer></footer>
+    </div>
+  );
 }
 
 export default App;
-
